db2255e8a4de36a702e8adb95123770871552d94
Gordon <gordonmikkelsen87@gmail.com>
2020-04-22 16:45:06 +0200

    dsds

--- SearchingShakespeare/Node.cs
+++ SearchingShakespeare/Node.cs
@@ -6,12 +6,9 @@ namespace SearchingShakespeare
 {
    public abstract class Node
     {
-        
-
        public abstract void Add(Key key, int value);
        public abstract int Find(Key key);
        public abstract Node Locate(Key key);
-
     }
 }
     
